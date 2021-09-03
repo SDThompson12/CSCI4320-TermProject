999f56ea752095dc3abfd7cef88f92656773bc13
hmarin7755 <Hmarin2698@gmail.com>
2021-09-02 23:13:47 -0400

    Updated Person5.java, implemented calc method

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person5.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.clayton.csit.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Qu
+ *  @author Humberto Marin
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,28 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+
+		String str = input;
+
+		char[] charArray = str.toCharArray();
+		int size = charArray.length;
+
+		for (int i = 0 ; i <= 2 ; i++) {
+			char first = charArray[0];
+			for (int j = 0; j < size - 1; j++) {
+				charArray[j] = charArray[j + 1];
+			}
+			charArray[size - 1] = first;
+		}
+
+
+		StringBuffer sb = new StringBuffer();
+		for (char c : charArray) {
+			sb.append(c);
+		}
+		//System.out.println(sb);
+
+	  return sb.toString();
 	}
 	
 	/**
